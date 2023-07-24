40f5d41c26d0ce392c9bcfb8129e5c9d0f696676
andywestthorp <andrew.graeme.westthorp@gmail.com>
2023-07-24 14:39:03 +0100

    New version with clickable pupils
    Added display list of pupils and clik on each to get more details.

--- firebase.js
+++ firebase.js
@@ -1,16 +1,45 @@
+// Import the functions you need from the SDKs you need
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-  import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut,  sendPasswordResetEmail, confirmPasswordReset } from 'https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js'
-    import { getFirestore, doc, getDocs } from 'https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js'
-      
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import {
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+    sendPasswordResetEmail,
+    confirmPasswordReset,
+    GoogleAuthProvider,
+    signInWithPopup
+} from 'https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js';
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+
+import {
+    getFirestore,
+    collection,
+    doc,
+    onSnapshot,
+    getDocs,
+    addDoc,
+    deleteDoc,
+    query,
+    where,
+    orderBy,
+    getDoc,
+    updateDoc
+} from 'https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js';
+
+import {
+    getAnalytics
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
     apiKey: "AIzaSyCfCLsX_gImU0zziR_0m4jS3PmLdK-g5xs",
     authDomain: "stu-s-driving-school.firebaseapp.com",
     databaseURL: "https://stu-s-driving-school-default-rtdb.europe-west1.firebasedatabase.app",
@@ -19,95 +48,234 @@
     messagingSenderId: "492596404048",
     appId: "1:492596404048:web:cde58c675ee12ec8395655",
     measurementId: "G-JRJWQ77L7R"
-  };
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-  // Initialize Firebase Authentication and get a reference to the service
-  const auth = getAuth(app);
-  signInWithEmailAndPassword(auth, email, password)
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      // ...
+        // Signed in 
+        const user = userCredential.user;
+        // ...
     })
     .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
+        const errorCode = error.code;
+        const errorMessage = error.message;
     });
-  
-  const notlog = document.getElementById('not-log-in');
-  const login = document.getElementById('logged-in');
-  const form = document.getElementById('form')  
-    
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        // const uid = user.uid;
-        // Display page
-  document.getElementById('footer').hidden = true;
-  document.getElementById('pupils').hidden = true;
-  document.getElementById('schedule').hidden = true;
-  document.getElementById('home').hidden = false;
-  document.getElementById('finance').hidden = true;
 
-  document.getElementById('splashPage').hidden = true;
-  document.getElementById('footer').hidden = false;
+const provider = new GoogleAuthProvider();
 
+const notlog = document.getElementById('not-log-in');
+const login = document.getElementById('logged-in');
+const form = document.getElementById('form');
+
+
+
+
+
+
+// Start of loggin in code...
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log('logged in!');
 
         showHome();
-        
-      } else {
-        // User is signed out
-        // ...
-        // Display page
-  document.getElementById('page').hidden = true;
-  document.getElementById('splashPage').hidden = false;
-        login.style.display = 'block'
-        notlog.style.display = 'block'
-      }
-    });
-    
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-    
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        console.log(email," ",password)
-    
-       signInWithEmailAndPassword(auth, email, password)
+
+
+    } else {
+        console.log('Not logged in, yet!');
+        document.getElementById('splashPage').hidden = false;
+
+    }
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    console.log(email, " ", password);
+
+    signInWithEmailAndPassword(auth, email, password)
         .then((user) => {
-            // Signed in 
-            // const user = usercred.user;
-            if (user) {
-              alert('sucess')
-            }
-            // ...
+
         })
         .catch((error) => {
-            console.log('error',error)
+            console.log('error', error);
         });
-    })
-    
+});
+
+// End of loggin in code...
+
+// Log out
+const logoutButton = document.querySelector('.logout');
+logoutButton.addEventListener('click', () => {
+    signOut(auth)
+        .then(() => {
+            console.log("Signed out");
+
+            // Change Display
+            showSplash();
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+});
+// end of log out
+
+// Sync Calendar
+const syncCalButton = document.querySelector('.syncCal');
+syncCalButton.addEventListener('click', () => {
+    console.log("Synchronise with Google Calendar");
+    alert('A possible feature to add?');
+});
+
+//
+// Elements for pupil data
+const pupilForeNameElement = document.getElementById("displayPupilForename");
+const pupilSurNameElement = document.getElementById("displayPupilSurname");
+// .. Add rest later!
+//
+// Start of database manipulations
+
 const db = getFirestore(app);
+const pupilsCol = collection(db, "pupils");
+// real time collection data
+
+// Queries
+const pupilQuery = query(pupilsCol, orderBy('surName', 'asc'));
+//const pupilQuery= query(pupilsCol, where("foreName","==","Lucy"), orderBy('surName','asc'))
+
+let pupilsList = [];
+
+
+onSnapshot(pupilQuery, (snapshot) => {
+
+    snapshot.docs.forEach((doc) => {
+        pupilsList.push({...doc.data(),
+            id: doc.id
+        });
+
+        let feed = document.createElement("div");
+        feed.setAttribute("class", "bg-theme mx-3 rounded-m shadow-m mt-3 mb-3");
+        feed.setAttribute("id", doc.id);
+
+      // This seems wrong! - why can't we pass an object?- doc.data()
+
+    
+      
+        let docLink = "displayPupilDetails(\'" + doc.data().active+"','"+ doc.data().addressLine1 + "','"+ doc.data().addressLine2 + "','"+ doc.data().city + "','"+ doc.data().county + "','"+ doc.data().dob + "','"+ doc.data().drivingLicenseNumber + "','"+ doc.data().email + "','"+ doc.data().foreName + "','"+ doc.data().postCode + "','"+ doc.data().surName + "','" + doc.data().telephone + "','" + doc.data().town + "\')";
+
+      
+      //console.log(docLink);
+      
+        let preText='<div onclick="'+docLink+'" class="d-flex px-2 pb-2 pt-2"><div><a  ><img src="Avatar-200.png" width="45" class="rounded-s" alt="img"></a></div><div class="ps-2 align-self-center"><h5 class="ps-1 mb-0 line-height-xs pt-1">';
+
+        let postText = '</div><div class="ms-auto"><a data-bs-toggle="dropdown" class="icon icon-m ps-3"><i class="bi bi-three-dots-vertical font-18 color-theme"></i></a></div>';
+
+        let phone = '<h6 class="ps-1 mb-0 line-height-xs pt-1">' + doc.data().telephone + '</h6>';
+        feed.innerHTML = preText + doc.data().foreName + "<br>" + doc.data().surName + phone + postText;
+
+        document.getElementById("feeds").append(feed);
+      
+    //  console.log(doc.data());
+      
+        /*
+      document.getElementByID(doc.id).addEventListener('click', e => {
+  e.preventDefault();
+  pupilsCol.doc(foreName.value).get()
+  .then(user => {
+    if(user.exists)
+      console.log(user.data());
+    else
+      console.log('User does not exist !');
+    })
+  .catch(error => {
+    console.error(error);
+  });
+});
+      */
+    });
+ //   console.log("Here is the data...");
+ //   console.log(pupilsList);
+
+});
 
 /*
-const pupilsCol = collection(db, "pupils");
-const docsSnap = await getDocs(pupilsCol);
+ // adding pupils
+const addPupilForm = document.querySelector('.add')
+addPupilForm.addEventListener('submit', (e) => {
+  e.preventDefault() // Prevent refresh of HTML page
 
-docsSnap.forEach(doc => {
-  console.log(doc.data());
+  addDoc(pupilsCol,{
+    foreName: addPupilForm.foreName.value,
+    surName: addPupilForm.surName.value
+  })
+  .then(() => {
+    addPupilForm.reset()
+     alert("Pupil's details have been added")
+  })
+
 })
-*/
 
-// Get a list of cities from your database
-async function getPupils() {
-  const pupilsCol = collection(db, 'pupils');
-  const pupilSnapshot = await getDocs(pupilsCol);
-  const pupilList = pupilSnapshot.docs.map(doc => doc.data());
-  return pupilList;
-}
-//console.log(app);
+// deleting a pupil record
+const deletePupilForm = document.querySelector('.delete')
+deletePupilForm.addEventListener('submit', (e) => {
+  e.preventDefault() // Prevent refresh of HTML page
+  const docRef = doc(db, "pupils", deletePupilForm.id.value);
+  deleteDoc(docRef)
+  .then(() => {
+    deletePupilForm.reset()
+    alert("Pupil's details have been removed")
+  })
+    
+})
+
+// get a single document
+const docRef = doc(db, 'pupils', '7q96KwNCrsb4NTXgXTxq')
+getDoc(docRef)
+.then((doc) => {
+  console.log(doc.data(), doc.id)
+})
+
+// updating a document
+const updatePupilForm = document.querySelector('.update')
+updatePupilForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const docRef = doc(db, "pupils", updatePupilForm.id.value);
+
+  updateDoc(docRef, {
+    foreName: 'Lucy'
+  })
+  .then(() => {
+    updatePupilForm.reset()
+  })
+  
+})
+
+// Finding a pupil
+
+const findPupilButton = document.querySelector('findPupilButton')
+findPupilButton.addEventListener('click', (e) => {
+  e.preventDefault() // Prevent refresh of HTML page
+  alert("Boo")
+  const dbref = ref(db)
+
+  get(child(dbref,"pupils/" + findPupilName.value))
+  .then((snapshot)=>{
+    if (snapshot.exists()){
+      //Display details  
+      displayPupilForename.innerHTML = snapshot.val().foreName;
+      displayPupilSurname.innerHTML = snapshot.val().surName;
+      
+    }
+    
+  })
+})
+
+*/
--- script.js
+++ script.js
@@ -10,11 +10,6 @@
     var pwaLocation = "/_service-worker.js";
 
 
-// Find pupil search bar
-function findPupil() {
-  // This will display a list of potential pupils
-  alert("Boo");
-}
 
 // Light and Dark mode switching
 function switchLightMode() {
@@ -39,13 +34,17 @@ function showPupils() {
   document.getElementById('schedule').hidden = true;
   document.getElementById('home').hidden = true;
   
-   document.getElementById('finance').hidden = true;
+  document.getElementById('finance').hidden = true;
+  document.getElementById('menu').hidden = true;
+
+   document.getElementById('pupilForm').hidden = true;
 
   // Reflect in footer menu
   document.getElementById('pupilsMenu').classList.add('active-nav');
   document.getElementById('homeMenu').classList.remove('active-nav');
   document.getElementById('scheduleMenu').classList.remove('active-nav');
   document.getElementById('financeMenu').classList.remove('active-nav');
+  document.getElementById('mainMenu').classList.remove('active-nav');
 
   }
 
@@ -56,6 +55,10 @@ function showSchedule() {
   document.getElementById('schedule').hidden = false;
   document.getElementById('home').hidden = true;
   document.getElementById('finance').hidden = true;
+    document.getElementById('menu').hidden = true;
+  
+   document.getElementById('pupilForm').hidden = true;
+
  
 
   // Reflect in footer menu
@@ -63,27 +66,43 @@ function showSchedule() {
   document.getElementById('homeMenu').classList.remove('active-nav');
   document.getElementById('financeMenu').classList.remove('active-nav');
   document.getElementById('scheduleMenu').classList.add('active-nav');
+  document.getElementById('mainMenu').classList.remove('active-nav');
   
   }
 
 
 function showSplash() {
   // Display page
+  document.getElementById('header').hidden = true;
   document.getElementById('footer').hidden = true;
   document.getElementById('pupils').hidden = true;
   document.getElementById('schedule').hidden = true;
   document.getElementById('home').hidden = true;
   document.getElementById('finance').hidden = true;
+  document.getElementById('menu').hidden = true;
+  
+   document.getElementById('pupilForm').hidden = true;
+  
 
   document.getElementById('splashPage').hidden = false;
 }
  
 function showHome() {
   // Display appropriate card
+  document.getElementById('header').hidden = false;
+  document.getElementById('footer').hidden = false;
+
+  document.getElementById('pupilForm').hidden = true;
+  
+  document.getElementById('splashPage').hidden = true;
+  
   document.getElementById('pupils').hidden = true;
   document.getElementById('schedule').hidden = true;
   document.getElementById('home').hidden = false;
   document.getElementById('finance').hidden = true;
+    document.getElementById('menu').hidden = true;
+   document.getElementById('pupilForm').hidden = true;
+
 
  
   // Reflect in footer menu
@@ -91,6 +110,7 @@ function showHome() {
   document.getElementById('scheduleMenu').classList.remove('active-nav');
   document.getElementById('homeMenu').classList.add('active-nav');
   document.getElementById('financeMenu').classList.remove('active-nav');
+  document.getElementById('mainMenu').classList.remove('active-nav');
   }
 
 
@@ -100,6 +120,9 @@ function showFinance() {
   document.getElementById('schedule').hidden = true;
   document.getElementById('home').hidden = true;
   document.getElementById('finance').hidden = false;
+  document.getElementById('menu').hidden = true;
+  document.getElementById('pupilForm').hidden = true;
+
   
   
   // Reflect in footer menu
@@ -107,7 +130,95 @@ function showFinance() {
   document.getElementById('scheduleMenu').classList.remove('active-nav');
   document.getElementById('homeMenu').classList.remove('active-nav');
   document.getElementById('financeMenu').classList.add('active-nav');
+  document.getElementById('mainMenu').classList.remove('active-nav');
+  
+
+
+}
+
+
+function showMenu() {
+  // Display appropriate card
+  document.getElementById('header').hidden = false;
+  document.getElementById('footer').hidden = false;
+  
+  document.getElementById('splashPage').hidden = true;
+  
+  document.getElementById('pupils').hidden = true;
+  document.getElementById('schedule').hidden = true;
+  document.getElementById('home').hidden = true;
+  document.getElementById('finance').hidden = true;
+  document.getElementById('menu').hidden = false;
+  
+  // Reflect in footer menu
+  document.getElementById('pupilsMenu').classList.remove('active-nav');
+  document.getElementById('scheduleMenu').classList.remove('active-nav');
+  document.getElementById('homeMenu').classList.remove('active-nav');
+  document.getElementById('financeMenu').classList.remove('active-nav');
+  document.getElementById('mainMenu').classList.add('active-nav');
   }
 
 
 
+function displayPupilDetails(active,addr1,addr2,city,county,dob,license,email,foreName,postcode,surName,telephone,town){
+
+ console.log(active);
+  console.log(active,addr1,addr2,city,county,dob,license,email,foreName,postcode,surName,telephone,town)
+
+  // Hide the big pupils list
+  document.getElementById('pupils').hidden = true;
+  // Show the individual pupil details
+ document.getElementById('pupilForm').hidden = false;
+  // If there is no data, the variable will have the value of "undefined"
+  if (foreName !=undefined){
+    document.getElementById('pupilFormForeName').innerHTML = foreName;
+  
+    if (surName !=undefined){
+      document.getElementById('pupilFormSurname').innerHTML = surName;
+      
+      // Set heading
+      document.getElementById('learnerHeading').innerHTML ='<img src="icons/icon-48x48.png">'+foreName+' '+surName;
+ //     document.getElementById('learnerHeading').innerHTML ='<img src="Avatar-200.png" width="45"><h1>'+foreName+' '+surName+'</h1>';
+     
+    }
+  }
+             
+   if (addr1 !=undefined){
+    document.getElementById('pupilFormAddr1').innerHTML = addr1;
+  }
+  if (addr2 !=undefined){
+    document.getElementById('pupilFormAddr2').innerHTML = addr2;
+  }
+   if (town !=undefined){
+    document.getElementById('pupilFormTown').innerHTML = town;
+  }
+  if (city !=undefined){
+    document.getElementById('pupilFormCity').innerHTML = city;
+  }
+   if (county !=undefined){
+    document.getElementById('pupilFormCounty').innerHTML = county;
+  }
+  if (postcode !=undefined){
+    document.getElementById('pupilFormPostcode').innerHTML = postcode;
+  }
+   if (telephone !=undefined){
+    document.getElementById('pupilFormTelephone').innerHTML = "Phone: "+telephone;
+  }
+  if (email !=undefined){
+    document.getElementById('pupilFormEmail').innerHTML = "Email: "+email;
+  }
+    if (dob !=undefined){
+      console.log(dob);
+    document.getElementById('pupilFormDOB').innerHTML = "DOB: "+dob;
+  }
+
+   if (license!=undefined){
+      console.log(dob);
+    document.getElementById('pupilFormLicense').innerHTML = "LicenseNo: "+license;
+  }
+   if (active !=undefined){
+      console.log(dob);
+    document.getElementById('pupilFormStatus').innerHTML = "Status: "+active;
+  }
+  
+}
